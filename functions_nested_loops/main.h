fca83f0dd492db8dfac9a3ed95c144d710f023c8
Eli6756 <elimalana765@gmail.com>
2025-02-20 09:17:45 +0000

    Write a program that prints _putchar, followed by a new line file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-#endif /* MAIN_H */
+return (0);
+}
