261afd24c819207aec8b37793e5fe4323f25b09f
Eli6756 <elimalana765@gmail.com>
2025-03-02 10:02:00 +0000

    Write a function that convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>  /* For INT_MAX and INT_MIN */
 
 /**
  * _atoi - Converts a string to an integer.
@@ -12,19 +13,25 @@ int i = 0, sign = 1, num = 0, started = 0;
 
 while (s[i] != '\0')
 {
-if (s[i] == '-')  /* Count negatives */
+if (s[i] == '-')  /* Handle negative sign */
 sign *= -1;
 
-if (s[i] >= '0' && s[i] <= '9')  /* If it's a number */
+if (s[i] >= '0' && s[i] <= '9')  /* If it's a digit */
 {
-num = num * 10 + (s[i] - '0');  /* Convert character to integer */
-started = 1;  /* Mark that number conversion has started */
+/* Check for overflow */
+if (num > (INT_MAX / 10) || (num == (INT_MAX / 10) && (s[i] - '0') > 7))
+{
+return (sign == 1 ? INT_MAX : INT_MIN);
 }
-else if (started)  /* Stop conversion if non-numeric char appears after numbers */
+
+num = num * 10 + (s[i] - '0');  /* Convert to integer */
+started = 1;
+}
+else if (started)  /* Stop at first non-digit after a number */
 break;
 
 i++;
 }
 
-return (num * sign);  /* Apply sign and return result */
+return (num * sign);
 }
