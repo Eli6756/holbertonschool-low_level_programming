facdf9e059edc297ee7f671bce1d1a1832259c59
Eli6756 <elimalana765@gmail.com>
2025-07-06 21:48:42 +0000

    Write a function that prints a square, followed by a new line

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ void print_diagonal(int n);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_square(int size);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
