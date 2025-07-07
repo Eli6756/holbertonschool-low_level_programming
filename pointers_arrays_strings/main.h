4e024dacb3cd09dc50bd42695aa388ea298208ce
Eli6756 <elimalana765@gmail.com>
2025-07-07 05:24:12 +0000

    Write a function that encodes a string into 1337

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
+char *leet(char *str);
 
 #endif /* MAIN_H */
