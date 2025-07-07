0c57dd841c9e9d70455257c6a968364e4db6bdfa
Eli6756 <elimalana765@gmail.com>
2025-07-07 04:45:02 +0000

    Write a function that compares two strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void print_rev(char *s);
 void _puts(char *str);
 void rev_string(char *s);
 int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void puts2(char *str);
