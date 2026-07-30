c621fa28428a0730fb57139b44395c6a634eba7f
Eli6756 <elimalana765@gmail.com>
2026-07-30 03:38:37 +0000

    Write a function that prints all integers from n to 98, followed by a new line

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: the number to print
+ */
+static void print_number(int n)
+{
+unsigned int num;
+
+if (n < 0)
+{
+_putchar('-');
+num = -(unsigned int)n;
+}
+else
+{
+num = (unsigned int)n;
+}
+
+if (num / 10)
+print_number(num / 10);
+
+_putchar((num % 10) + '0');
+}
+
+/**
+ * print_to_98 - prints all integers from n to 98, followed by a new line
+ * @n: the starting number
+ */
+void print_to_98(int n)
+{
+if (n == 98)
+{
+print_number(n);
+_putchar('\n');
+return;
+}
+
+if (n < 98)
+{
+while (n <= 98)
+{
+print_number(n);
+if (n != 98)
+_putchar(',');
+_putchar(' ');
+n++;
+}
+}
+else
+{
+while (n >= 98)
+{
+print_number(n);
+if (n != 98)
+_putchar(',');
+_putchar(' ');
+n--;
+}
+}
+}
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
  *
- * Return: On success 1. On error, -1.
+ * Return: On success 1. On error, -1 is returned, and errno is set.
  */
 int _putchar(char c)
 {
