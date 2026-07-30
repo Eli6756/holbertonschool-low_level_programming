35e0f0ba9cc0485492b3455a58f3fe7fb75f9a95
Eli6756 <elimalana765@gmail.com>
2026-07-30 04:04:36 +0000

    Write a function that prints the n times table, starting with 0

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - prints a non-negative integer using _putchar
+ * @n: the number to print
+ */
+static void print_number(int n)
+{
+if (n / 10)
+print_number(n / 10);
+
+_putchar((n % 10) + '0');
+}
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: size of the table
+ */
+void print_times_table(int n)
+{
+int i, j;
+
+if (n > 15 || n < 0)
+return;
+
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+print_number(i * j);
+if (j != n - 1)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
